9d445048ded65d370388b29775024c64f7c9d923
hakolegag-commits <hakolegag@gmail.com>
2026-07-22 14:08:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ bot.start((ctx) => {
   ctx.reply('Привет! Добро пожаловать за столы Техасского Холдема! Делай ставки и побеждай.');
 });
 
-// Настройка вебхука для Render
 const PORT = process.env.PORT || 3000;
-const TOKEN = '8939019076:AAHTYvEE9VfYfl82epZlhEquPij3WzttMa4';
-const URL = process.env.RENDER_EXTERNAL_URL || `https://poker2026gemini-bot.onrender.com`;
 
-const server = http.createServer(bot.webhookCallback(`/${TOKEN}`));
+// Создаем сервер с простым путем /webhook
+const server = http.createServer(bot.webhookCallback('/webhook'));
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
   console.log(`Server is listening on port ${PORT}`);
-  bot.telegram.setWebhook(`${URL}/${TOKEN}`);
-  console.log('Webhook is set');
+  
+  // Указываем Telegram отправлять данные на /webhook
+  const URL = process.env.RENDER_EXTERNAL_URL || `https://poker2026gemini-bot.onrender.com`;
+  await bot.telegram.setWebhook(`${URL}/webhook`);
+  console.log('Webhook is automatically set!');
 });
